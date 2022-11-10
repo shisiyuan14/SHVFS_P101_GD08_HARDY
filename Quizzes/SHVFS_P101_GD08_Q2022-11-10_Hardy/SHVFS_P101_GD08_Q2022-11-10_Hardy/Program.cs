cec8956b7b6a11855ac8b00de2a6a0b36812a3f1
shisiyuan14 <46749408+shisiyuan14@users.noreply.github.com>
2022-11-10 20:24:45 +0800

    Add files via upload

---
+++ Quizzes/SHVFS_P101_GD08_Q2022-11-10_Hardy/SHVFS_P101_GD08_Q2022-11-10_Hardy/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO.Pipes;
+using System.Reflection.Metadata;
+
+namespace SHVFS_P101_GD08_Q4_Hardy
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            string a,answer;
+            int i = 0;
+            string[] names = { "Hardy", "Chris", "Bandy","April","Cloud","Matt" };
+            Console.WriteLine("Hello user! Let's play a word guessing game!");
+            Console.WriteLine("Please input your name!");
+            a=Console.ReadLine();
+            Console.WriteLine($"Ok,{a}. The game begin!");
+            do
+            {
+                
+                Console.WriteLine("Please guess the correct name from \"Hardy\", \"Chris\", \"Bandy\",\"April\",\"Cloud\",\"Matt\"");
+                answer = Console.ReadLine();
+                if(answer=="Hardy")
+                {
+                    Console.WriteLine("You got the answer!");
+                }
+                else if(answer !="Hardy")
+                {
+                    Console.WriteLine("Wrong!");
+                    Console.WriteLine("Please input anything to clear");
+                    Console.ReadLine();
+                    Console.Clear();
+                }
+                i=i+1;  
+            } while (answer != "Hardy");
+            
+            Console.WriteLine($"And you have used {i} times, great!");
+
+
+
+
+
+
+
+        }
+
+
+    }
+
+
+
+
+}
