19312f9c0594f6d4fbc1ebdf20f64a03cadc2282
shisiyuan14 <46749408+shisiyuan14@users.noreply.github.com>
2022-11-12 17:27:11 +0800

    Add files via upload

---
+++ Homework/SHVFS_P101_GD08_HW2_Hardy/SHVFS_P101_GD08_HW2_Hardy/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Diagnostics;
+using System.Security.Cryptography.X509Certificates;
+
+namespace SHVFS_P101_GD08_HW2_Hardy
+{
+    
+    public class program
+    {
+        
+        public static void Main(string[] args)
+        {
+            string f;
+            float a;
+            Console.WriteLine("       Fahrenheight Calculator!\n          ");
+            Console.WriteLine("Please input a Fahrenheight!");
+            f= Console.ReadLine();
+            var f1converted = Convert.ToSingle(f);
+            a = (f1converted - 32) * 5 / 9;
+            Console.WriteLine($"The celsius is {a.ToString("f2")}!");
+            
+
+
+
+        }
+        
+
+    }
+
+}
