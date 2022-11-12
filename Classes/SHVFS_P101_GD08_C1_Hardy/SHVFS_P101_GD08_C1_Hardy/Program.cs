b2b4a6d8094f16079bc6957e6fb6a1e2dacca766
shisiyuan14 <46749408+shisiyuan14@users.noreply.github.com>
2022-11-12 17:32:53 +0800

    Add files via upload

---
+++ Classes/SHVFS_P101_GD08_C1_Hardy/SHVFS_P101_GD08_C1_Hardy/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+#region 
+namespace SHVFS_P101_GD08_C1_Hardy
+{
+
+public class program
+{
+    public static void Main(string[]args)
+    {
+            int myFirstNumber = 5;
+            var mySecondNumber = 8;
+
+            //int类型的最大值:  2147483647,最小值:  -2147483648
+            Console.WriteLine($"My first number is:{myFirstNumber}");
+            Console.WriteLine($"My second number is:{mySecondNumber}");
+            Console.ReadLine();
+
+    }
+    }
+
+
+
+}
+
+#endregion
