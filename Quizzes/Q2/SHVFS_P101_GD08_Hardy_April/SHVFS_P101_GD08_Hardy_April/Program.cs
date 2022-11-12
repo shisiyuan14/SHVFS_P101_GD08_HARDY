2a56816f15484d83f72eb7d99c02e689740784bb
shisiyuan14 <46749408+shisiyuan14@users.noreply.github.com>
2022-11-12 17:24:20 +0800

    Add files via upload

---
+++ Quizzes/Q2/SHVFS_P101_GD08_Hardy_April/SHVFS_P101_GD08_Hardy_April/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace SHVFS_P101_GD08_Hardy_April
+{
+    public class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            float a;
+            float b;
+            while (true)
+            {
+                Console.WriteLine("Please input Dividend ");    
+                float.TryParse(Console.ReadLine(), out a);
+                Console.WriteLine("Please input Divisor ");
+                float.TryParse(Console.ReadLine(), out b);
+                if (b == 0)
+                {
+                    Console.WriteLine("Get Wrong!");
+                }
+                else
+                {
+                    Console.WriteLine($"The result is {a / b}");
+                }
+                //Console.Clear();
+            }
+
+        }
+    }
+}
